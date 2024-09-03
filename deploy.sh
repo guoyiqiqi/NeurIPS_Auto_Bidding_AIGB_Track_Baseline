036ced5f3bd34b0ff4c464fd43aebd936238a5f7
171717！ <934287495@qq.com>
2024-09-03 21:41:18 +0800

    [Modify]

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-sudo docker build -t registry.cn-beijing.aliyuncs.com/nips_train_env_yq/train_env:duqiao_test_td3 -f ./Dockerfile .
-sudo docker push registry.cn-beijing.aliyuncs.com/nips_train_env_yq/train_env:duqiao_test_td3
+docker build -t registry.cn-beijing.aliyuncs.com/nips_train_env_yq/train_env:yq_test1 -f ./Dockerfile .
+docker push registry.cn-beijing.aliyuncs.com/nips_train_env_yq/train_env:yq_test1
 
 
